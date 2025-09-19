940e4bf0e8e954df138c693ce7ce5fd35cb8669f
fish0707 <84836857+fish0707@users.noreply.github.com>
2025-09-19 11:53:16 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/AI-/", // 添加這一行
+  plugins: [react()],
+})
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
